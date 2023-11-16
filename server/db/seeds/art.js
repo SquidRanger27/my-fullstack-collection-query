fd2bb466af1341999e9b1506709ead8b4e120334
student <student@pop-os.localdomain>
2023-11-16 14:57:01 +1300

    made art tables

---
+++ server/db/seeds/art.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('art').del()
+  await knex('art').insert([
+    {id: 1, name: 'The Wave', description: 'I did this collage at my parents house. The bird on the boat was an accident at first but I love him', medium: 'collage', imageUrl: 'public/wave.jpeg', owner:'rose'},
+    {id: 2, name: 'Youth', description:'A basset hound dog walked into my house half way through making this piece of art. Gaby was crafting with me at the time.', medium: 'collage', imageUrl:'public/Klint.jpeg', owner:'rose'},
+    {id: 3, name: 'Cass', description:'I made this based on a request from dad. It was when I began to take it more seriously.', medium: 'collage', imageUrl: 'public/angus.jpeg', owner:'rose' }
+  ]);
+}
