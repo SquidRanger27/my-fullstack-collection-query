0c26db4a42b4efef0e285043c328468db561b146
Sofia Etchegoin <142562661+sofia-etchegoin@users.noreply.github.com>
2023-11-16 14:32:35 +1300

    set up migration table for books

--- server/db/migrations/20231116011912_books.js
+++ server/db/migrations/20231116011912_books.js
@@ -2,10 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export function up(knex) {}
+export function up(knex) {
+  return knex.schema.createTable('books', (table) => {
+    table.increments('id')
+    table.varchar('title')
+    table.varchar('author')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export function down(knex) {}
+export function down(knex) {
+  return knex.schema.dropTable('books')
+}
