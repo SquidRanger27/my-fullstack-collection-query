5c4375e1b3cb04510201d172234f648637a6ce30
student <student@pop-os.localdomain>
2023-11-16 14:33:29 +1300

    create art table migration

---
+++ server/db/migrations/20231116012407_art.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('art', function(table){
+    table.increments('id')
+    table.string('name')
+    table.string('description')
+    table.string('medium')
+    table.string('imageUrl')
+    table.string('owner')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('art')
+}
