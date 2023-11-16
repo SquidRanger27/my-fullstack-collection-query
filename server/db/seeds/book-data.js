b2f758fd084d30310b09f201762b9ba8aee40159
Sofia Etchegoin <142562661+sofia-etchegoin@users.noreply.github.com>
2023-11-16 14:42:04 +1300

    seeded the table

---
+++ server/db/seeds/book-data.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('books').del()
+  await knex('books').insert([
+    { id: 1, title: 'A Christmas Carol', author: 'Charles Dickens' },
+    { id: 2, title: 'Law in a Time of Crises', author: 'Jonathan Sumption' },
+    { id: 3, title: 'Emma', author: 'Jane Austen' },
+  ])
+}
