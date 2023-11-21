e870e4560ebd1c845048caa3ba83dfd7ca4e1ce4
williford abunas <abunaswilliford.com>
2023-11-21 15:27:17 +1300

    fix client side routing

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,8 +1,7 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 
-import HomePage from './components/AppLayout'
+import HomePage from './components/HomePage'
 import AppLayout from './components/AppLayout'
-
 export const routes = createRoutesFromElements(
   <Route element={<AppLayout />}>
     <Route index element={<HomePage />} />
