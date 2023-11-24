bab3af81da39bb0d3c7cf9d1d24f25821219980c
Arianne Hallgarth <alvarezarianne15@yahoo.com>
2023-11-25 01:34:57 +1300

    Created database table

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,7 +2,9 @@ function App() {
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        <h1 className="title">
+          <em>The Essential Jesus Challenge:</em>
+        </h1>
       </header>
       <section className="main">{/* add your code here */}</section>
     </>
---
+++ server/db/migrations/20231124122433_verses.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('verses', (table) => {
+    table.increments('id')
+    table.string('description')
+    table.string('verse')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('verses')
+}
