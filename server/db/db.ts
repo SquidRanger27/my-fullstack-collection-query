1f1cbd58d257a337b7a50efe633d41cfd347b861
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-11-25 10:30:31 +1300

    Database functions updated

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,10 +1,23 @@
 import knexfile from './knexfile.js'
 import knex from 'knex'
+import { CharacterModel, NewCharacterModel } from '../../models/Character.js'
 
 const db = knex(knexfile.development)
 
 export async function getCharacters() {
   return db.table('characters').select()
+}
+
+export async function deleteCharacter(id: number) {
+  return db.table('characters').delete().where('id', id)
+}
+
+export async function addCharacter(newCharacter: NewCharacterModel) {
+  return db.table('characters').insert(newCharacter)
+}
+
+export async function updateCharacter(character: CharacterModel) {
+  return db.table('characters').update(character).where('id', character.id)
 }
 
 export function close() {
