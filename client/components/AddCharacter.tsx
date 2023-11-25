d38d3aa628a601e1150a3546cad241381c53b7e8
Tristan Bulmer <63182722+Hoffee-Toffee@users.noreply.github.com>
2023-11-25 14:16:33 +1300

    Add Character functionality

--- client/components/AddCharacter.tsx
+++ client/components/AddCharacter.tsx
@@ -1,10 +1,76 @@
 import { Link } from 'react-router-dom'
 
+import { useState } from 'react'
+import { addCharacter } from '../apis/characters'
+
 function AddCharacter() {
+  const [characterData, setCharacterData] = useState({
+    alias: '',
+    name: '',
+    image: '',
+  })
+  const [adding, setAdding] = useState(false)
+
+  function toggleAdd() {
+    setAdding(!adding)
+  }
+
+  function handleAdd(event: { preventDefault: () => void }) {
+    event.preventDefault()
+    // Show error message if both name and alias are null
+    if (characterData.name || characterData.alias) {
+      addCharacter(characterData)
+      setAdding(false)
+    } else {
+      alert("At least one of 'Alias' and 'Name' must be defined.")
+    }
+  }
+
+  function handleChange(event: { target: HTMLInputElement }) {
+    const el = event.target
+    setCharacterData({ ...characterData, [el.name]: el.value || null })
+  }
+
+  if (adding) {
+    return (
+      <>
+        <form className="character" onSubmit={handleAdd}>
+          <h2>
+            <label htmlFor="alias">Alias:</label>
+            <input
+              onChange={handleChange}
+              name="alias"
+              value={characterData.alias || ''}
+            />
+          </h2>
+          <h3>
+            <label htmlFor="name">Name:</label>
+            <input
+              onChange={handleChange}
+              name="name"
+              value={characterData.name || ''}
+            />
+          </h3>
+          <h4>
+            <label htmlFor="image">Image URL:</label>
+            <input
+              onChange={handleChange}
+              name="image"
+              value={characterData.image || ''}
+            />
+          </h4>
+          <div>
+            <input type="submit" value="Add Character" />
+            <button onClick={toggleAdd}>Cancel</button>
+          </div>
+        </form>
+      </>
+    )
+  }
   return (
     <>
       <div className="character add">
-        <Link to="./add">+</Link>
+        <button onClick={toggleAdd}>+</button>
       </div>
     </>
   )
