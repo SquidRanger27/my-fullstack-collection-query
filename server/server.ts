03f2cc1cb73457a08ae1adba55159bcce75b4c17
Cody Ilg <codyilg@gmail.com>
2023-11-28 19:45:08 +1300

    configuring for dokku

--- server/server.ts
+++ server/server.ts
@@ -15,4 +15,12 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(Path.resolve('public')))
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
+  })
+}
+
 export default server
