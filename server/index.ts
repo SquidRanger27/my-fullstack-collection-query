069c4acf36d89f939c9892b0fb4d81374aa2fd6b
student <student@pop-os.localdomain>
2023-11-28 20:52:01 +1300

    dokku ts

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,6 @@
 import server from './server'
-import * as Path from 'node:path'
-import express from 'express'
 
-if (process.env.NODE_ENV === 'production') {
-  server.use(express.static(Path.resolve('public')))
-  server.use('/assets', express.static(Path.resolve('./dist/assets')))
-  server.get('*', (req, res) => {
-    res.sendFile(Path.resolve('./dist/index.html'))
-  })
-}
+
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
